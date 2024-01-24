b8cd44f4458b84ac3f862bfd353985dedf367d65
Bruno Rogalski <brunorek10@gmail.com>
2024-01-24 20:11:45 +0100

    Added language switch button and a history page draft

---
+++ TransLib/Translation.cs
@@ -0,0 +1,10 @@
+namespace TransLib
+{
+    public class Translation
+    {
+        public string inputLanguage;
+        public string input;
+        public string outputLanguage;
+        public string output;
+    }
+}
