10d39e397c4a44f99659694453a95e8cf8924760
shef3r <brunorek10@gmail.com>
2024-01-30 16:39:13 +0100

    made copying in history work!! :33

--- SubPages/HistoryPage.xaml.cs
+++ SubPages/HistoryPage.xaml.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
 using TransLib;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -24,6 +27,14 @@ namespace Transhef.SubPages
         {
             this.InitializeComponent();
             historyList.ItemsSource = Methods.RetrieveHistory();
+        }
+        private void Copy(object sender, RoutedEventArgs e)
+        {
+            string text = ((sender as AppBarButton).CommandParameter as string);
+            System.Diagnostics.Debug.WriteLine(text);
+            DataPackage dataPackage = new DataPackage();
+            dataPackage.SetText(text);
+            Clipboard.SetContent(dataPackage);
         }
     }
 }
